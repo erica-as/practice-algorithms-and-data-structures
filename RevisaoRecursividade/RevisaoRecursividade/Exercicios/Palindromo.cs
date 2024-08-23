a15d7e6ac91c090af7a3ad75912deac8e2c302d7
erica-as <erica.asnt@hotmail.com>
2024-08-22 22:31:55 -0300

    prática: exercicio palindromo

--- RevisaoRecursividade/RevisaoRecursividade/Exercicios/Palindromo.cs
+++ RevisaoRecursividade/RevisaoRecursividade/Exercicios/Palindromo.cs
@@ -1,7 +1,37 @@
-﻿namespace RevisaoRecursividade.Exercicios
+﻿using System;
+
+namespace RevisaoRecursividade.Exercicios
 {
     public class Palindromo
     {
-        
+        private static bool VerificarPalindromoRecursivo(string entrada)
+        {
+            if (entrada.Length <= 1) {
+                return true;
+            }
+
+            if (entrada[0] != entrada[entrada.Length - 1]) {
+                return false;
+            }
+
+            return VerificarPalindromoRecursivo(entrada.Substring(1, entrada.Length - 2));
+        }
+
+        public static void Main(string[] args)
+        {
+            while (true) {
+                string entrada = Console.ReadLine();
+                if (entrada.Equals("FIM")) {
+                    break;
+                }
+
+                if (VerificarPalindromoRecursivo(entrada)) {
+                    Console.WriteLine("SIM");
+                } else {
+                    Console.WriteLine("NAO");
+                }
+            }
+        }
     }
+
 }
